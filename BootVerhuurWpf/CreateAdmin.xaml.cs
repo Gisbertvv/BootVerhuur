965e7b12a59f69e41936dcf3d41430e4c722aa64
Rosalie03 <s1173877@student.windesheim.nl>
2022-11-25 16:01:59 +0100

    Create Admin window, classes,, Created checks for email and empty fields, buttons still for testing can be removed later.

--- BootVerhuurWpf/CreateAdmin.xaml.cs
+++ BootVerhuurWpf/CreateAdmin.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,9 +21,104 @@ namespace BootVerhuurWpf
     /// </summary>
     public partial class CreateAdmin : Window
     {
+        public Admin admin { get; set; }
         public CreateAdmin()
         {
             InitializeComponent();
+        }
+
+        private static bool IsEmailValid(string email)
+        {
+            var valid = true;
+
+            try
+            {
+                var emailAddress = new MailAddress(email);
+            }
+            catch
+            {
+                valid = false;
+            }
+
+            return valid;
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            CreateMember createMember = new CreateMember();
+            createMember.Show();
+            this.Close();
+        }
+
+        private void Create_Admin(object sender, RoutedEventArgs e)
+        {
+            string messageBoxText;
+            string caption;
+            MessageBoxButton button;
+            MessageBoxImage icon;
+            MessageBoxResult result;
+
+            bool digits = false;
+            bool special = false;
+
+            string regexItem = @"\|!#$%&/+-()=?»«@£§€{}.-;'<>_,";
+
+           
+            foreach (char l in txtWachtwoord.Text)
+            {
+               if (Char.IsDigit(l))
+                {
+                    digits = true;
+                }
+
+                if (digits)
+                {
+                    break;
+                }
+            }
+
+            foreach( var item in regexItem)
+            {    
+
+                if (txtWachtwoord.Text.Contains(item))
+                {
+                    special = true;
+                }
+                if (special)
+                {
+                    break;
+                }
+            }
+            if (!IsEmailValid(txtEmail.Text) && !txtEmail.Text.EndsWith(".nl") && !txtEmail.Text.EndsWith(".com"))
+            {
+                messageBoxText = "Email is ongeldig";
+                caption = "FAILED: Email Ongeldig";
+                button = MessageBoxButton.OK;
+                icon = MessageBoxImage.Error;
+            }
+            else if (!digits || !special || txtWachtwoord.Text.Length <= 7)
+            {
+                messageBoxText = "Wachtwoord moet 8 tekens lang zijn, een cijfer en een speciale teken bevatten";
+                caption = "FAILD: Wachtwoord voldoet niet aan de eisen";
+                button = MessageBoxButton.OK;
+                icon = MessageBoxImage.Error;
+            }
+            else if (string.IsNullOrWhiteSpace(txtGebruikersnaam.Text) || string.IsNullOrWhiteSpace(txtEmail.Text) || string.IsNullOrWhiteSpace(txtWachtwoord.Text))
+            {
+                messageBoxText = "Alle velden moeten ingevuld zijn";
+                caption = "FAILED: Één of meerdere velden zijn leeg";
+                button = MessageBoxButton.OK;
+                icon = MessageBoxImage.Error;
+            }
+            else
+            {
+                admin = new Admin(txtGebruikersnaam.Text, txtWachtwoord.Text, Rol.Text, txtEmail.Text);
+                messageBoxText = "Admin is aangemaakt";
+                caption = "SUCCES";
+                button = MessageBoxButton.OK;
+                icon = MessageBoxImage.Information;
+            }
+            result = MessageBox.Show(messageBoxText, caption, button, icon);
         }
     }
 }
--- BootVerhuurWpf/CreateMember.xaml.cs
+++ BootVerhuurWpf/CreateMember.xaml.cs
@@ -1,7 +1,7 @@
-﻿using BootVerhuur;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,23 +12,36 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using System.Xml.Linq;
 
 namespace BootVerhuurWpf
 {
+    
     /// <summary>
     /// Interaction logic for CreateMember.xaml
     /// </summary>
     public partial class CreateMember : Window
     {
-
         public Member Lid;
-
         public CreateMember()
         {
             InitializeComponent();
         }
 
+        private static bool IsEmailValid(string email)
+        {
+            var valid = true;
+
+            try
+            {
+                var emailAddress = new MailAddress(email);
+            }
+            catch
+            {
+                valid = false;
+            }
+
+            return valid;
+        }
         private void ButtonAddUser_Click(object sender, RoutedEventArgs e)
         {
             string messageBoxText;
@@ -37,29 +50,73 @@ namespace BootVerhuurWpf
             MessageBoxImage icon;
             MessageBoxResult result;
 
-            if (!string.IsNullOrWhiteSpace(txtVoornaam.Text) && !string.IsNullOrWhiteSpace(txtAchternaam.Text) &&
-                !string.IsNullOrWhiteSpace(txtGebruikersnaam.Text) && !string.IsNullOrWhiteSpace(txtEmail.Text) &&
-                !string.IsNullOrWhiteSpace(_txtTelefoonnummer.Text) && !string.IsNullOrWhiteSpace(Rol.Text) &&
-                !string.IsNullOrWhiteSpace(Niveau.Text) && txtEmail.Text.Contains('@') 
-                && txtEmail.Text.EndsWith(".nl") || txtEmail.Text.EndsWith(".com"))  
-            {
-                Lid = new Member(txtVoornaam.Text, txtAchternaam.Text, txtGebruikersnaam.Text, txtWachtwoord.Text, txtEmail.Text, _txtTelefoonnummer.Text, Rol.Text, Niveau.Text);
+            bool digits = false;
+            bool special = false;
 
-               messageBoxText = "User is aangemaakt";
-                caption = "Succes";
-                 button = MessageBoxButton.OK;
-               icon = MessageBoxImage.Information;                           
+            string regexItem = @"\|!#$%&/+-()=?»«@£§€{}.-;'<>_,";
+
+
+            foreach (char l in txtWachtwoord.Text)
+            {
+                if (Char.IsDigit(l))
+                {
+                    digits = true;
+                }
+
+                if (digits)
+                {
+                    break;
+                }
+            }
+
+            foreach (var item in regexItem)
+            {
+
+                if (txtWachtwoord.Text.Contains(item))
+                {
+                    special = true;
+                }
+                if (special)
+                {
+                    break;
+                }
+            }
+
+            if (!IsEmailValid(txtEmail.Text)&& !txtEmail.Text.EndsWith(".nl") && !txtEmail.Text.EndsWith(".com"))
+            {
+                messageBoxText = "Email is ongeldig";
+                caption = "FAILED: Email Ongeldig";
+                button = MessageBoxButton.OK;
+                icon = MessageBoxImage.Error;
+            }                       
+            else if(string.IsNullOrWhiteSpace(txtVoornaam.Text) || string.IsNullOrWhiteSpace(txtAchternaam.Text) ||
+                            string.IsNullOrWhiteSpace(txtGebruikersnaam.Text) || string.IsNullOrWhiteSpace(txtEmail.Text) ||
+                            string.IsNullOrWhiteSpace(_txtTelefoonnummer.Text) || string.IsNullOrWhiteSpace(Rol.Text) || string.IsNullOrWhiteSpace(Niveau.Text))
+            {
+                messageBoxText = "Alle velden moeten ingevuld zijn";
+                caption = "FAILED: Één of meerdere velden zijn leeg";
+                button = MessageBoxButton.OK;
+                icon = MessageBoxImage.Error;
+            }
+            else if (!digits || !special || txtWachtwoord.Text.Length <= 7)
+            {
+                messageBoxText = "Wachtwoord moet 8 tekens lang zijn, een cijfer en een speciale teken bevatten";
+                caption = "FAILED: Wachtwoord voldoet niet aan de eisen";
+                button = MessageBoxButton.OK;
+                icon = MessageBoxImage.Error;
             }
             else
             {
-                 messageBoxText = "Een veld is niet goed ingevoerd";
-                 caption = "Failed";
-                 button = MessageBoxButton.OK;
-                 icon = MessageBoxImage.Error;                 
+                Lid = new Member(txtVoornaam.Text, txtAchternaam.Text, txtGebruikersnaam.Text, txtWachtwoord.Text, txtEmail.Text, _txtTelefoonnummer.Text, Rol.Text, Niveau.Text);
+                messageBoxText = "User is aangemaakt";
+                caption = "SUCCES";
+                button = MessageBoxButton.OK;
+                icon = MessageBoxImage.Information;
             }
-               result = MessageBox.Show(messageBoxText, caption, button, icon, MessageBoxResult.OK);
-            }
-
+            result = MessageBox.Show(messageBoxText, caption, button, icon, MessageBoxResult.OK);
+        }
+        
+        
         private void Check_Admin(object sender, RoutedEventArgs e)
         {
             if (Rol.Text.Equals("Admin"))
@@ -69,7 +126,13 @@ namespace BootVerhuurWpf
                 this.Close();
             }
         }
-    }
 
-    
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow= new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+    }
 }
+
