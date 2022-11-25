aeb1ba021a7045eef2a97e6fa15a9152cd7e31a6
Daan <s1168181@student.windesheim.nl>
2022-11-25 12:14:15 +0100

    Restored files from stash
    Restored files from stash. Don't know what it will do to the app.

---
+++ UploadFile/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UploadFile
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string Name { get; set; } = "Daan";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = this;
+        }
+
+        private void btnUploadImage(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                if (ofd.ShowDialog == ofd.FileOk {
+
+                }
+                var fileName = System.IO.Path.GetFileName(ofd.FileName);
+                
+            }
+        }
+    }
+}
