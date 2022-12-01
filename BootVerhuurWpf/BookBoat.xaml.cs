0c47dbadd3f2775e3478deb460190320c88c71f6
Rosalie03 <s1173877@student.windesheim.nl>
2022-12-01 16:24:07 +0100

    bookwindow created

---
+++ BootVerhuurWpf/BookBoat.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace BootVerhuurWpf
+{
+    /// <summary>
+    /// Interaction logic for BookBoat.xaml
+    /// </summary>
+    public partial class BookBoat : Window
+    {
+        public BookBoat()
+        {
+            InitializeComponent();
+            AdjustCalender();
+            AdjustTimeBox();
+
+        }
+        /// <summary>
+        /// maybe change to get availeble time from database, or set time each 2 hours
+        /// </summary>
+        private void AdjustTimeBox()
+        {
+            int timecount =0;
+            Gekozentijd.Items.Add($"0{timecount}:00");
+            timecount = timecount + 2;
+            Gekozentijd.Items.Add($"0{timecount}:00");
+            timecount += 2;
+            Gekozentijd.Items.Add($"0{timecount}:00");
+            timecount += 2;
+            Gekozentijd.Items.Add($"0{timecount}:00");
+            timecount += 2;
+            Gekozentijd.Items.Add($"0{timecount}:00");
+        }
+
+        
+
+        private void AdjustCalender()
+        {
+            DP.DisplayDateStart = DateTime.Now;
+            DateTime plusone = DateTime.Now.AddDays(1);
+            DateTime plustwo = DateTime.Now.AddDays(2);
+
+            if (plusone.DayOfWeek == DayOfWeek.Saturday)
+            {
+                DP.BlackoutDates.Add(new CalendarDateRange(DateTime.Now.AddDays(1), DateTime.Now.AddDays(2)));
+                DP.DisplayDateEnd = DateTime.Now.AddDays(3);
+            }
+            else if (plustwo.DayOfWeek == DayOfWeek.Saturday)
+            {
+                DP.BlackoutDates.Add(new CalendarDateRange(DateTime.Now.AddDays(2), DateTime.Now.AddDays(3)));
+                DP.DisplayDateEnd = DateTime.Now.AddDays(4);
+            }
+            else if (DateTime.Now.DayOfWeek == DayOfWeek.Saturday)
+            {
+                DP.BlackoutDates.Add(new CalendarDateRange(DateTime.Now, DateTime.Now.AddDays(1)));
+                DP.DisplayDateEnd = DateTime.Now.AddDays(2);
+            }
+            else if (plustwo.DayOfWeek == DayOfWeek.Sunday)
+            {
+                DP.BlackoutDates.Add(new CalendarDateRange(DateTime.Now.AddDays(1), DateTime.Now.AddDays(2)));
+                DP.DisplayDateEnd = DateTime.Now.AddDays(3);
+            }
+            else if (plusone.DayOfWeek == DayOfWeek.Sunday)
+            {
+                DP.BlackoutDates.Add(new CalendarDateRange(DateTime.Now, DateTime.Now.AddDays(1)));
+                DP.DisplayDateEnd = DateTime.Now.AddDays(2);
+            }
+            else if (DateTime.Now.DayOfWeek == DayOfWeek.Sunday)
+            {
+                DP.BlackoutDates.Add(new CalendarDateRange(DateTime.Now, DateTime.Now.AddDays(-1)));
+                DP.DisplayDateEnd = DateTime.Now.AddDays(2);
+            }
+        }
+
+        private void Logout(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Book(object sender, RoutedEventArgs e)
+        {
+            //dont know yet if correct
+            //set reservation in database or in another class
+            string selecteddate = DP.SelectedDate.Value.Date.ToShortDateString();
+            String selectedtime = Gekozentijd.Text;          
+        }
+
+        private void Status(object sender, RoutedEventArgs e)
+        {
+            var label = sender as Label;
+            label.Content = $"Status : ";
+
+        }
+
+        private void AantalPersonen(object sender, RoutedEventArgs e)
+        {
+            var label = sender as Label;
+            label.Content = $"Aantal Personene : ";
+        }
+
+        private void Stuur(object sender, RoutedEventArgs e)
+        {
+            var label = sender as Label;
+            label.Content = $"Stuur : ";
+        }
+
+        private void BootNiveau(object sender, RoutedEventArgs e)
+        {
+            var label = sender as Label;
+            label.Content = $"Niveau : ";
+        }
+
+        private void AvailibleFrom(object sender, RoutedEventArgs e)
+        {
+            //get time from database
+        }
+    }
+}
--- BootVerhuurWpf/MainWindow.xaml.cs
+++ BootVerhuurWpf/MainWindow.xaml.cs
@@ -14,15 +14,27 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace BootVerhuurWpf
-{
+{  
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
+    { 
+        BookBoat bookBoat = new BookBoat();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Logout(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Open_BookBoat(object sender, RoutedEventArgs e)
+        {
+            bookBoat.Show();
+            Close();
         }
     }
 }
