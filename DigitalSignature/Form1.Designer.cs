2bad8815ec655233bae5a6ed74c02445dd9a2bc5
Daan <s1168181@student.windesheim.nl>
2022-12-01 11:33:45 +0100

    DigitalSignature
    Created this new branch to create a digital signature. Works in Forms application, but haven't tested it in WPF.

---
+++ DigitalSignature/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace DigitalSignature
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel1.Location = new System.Drawing.Point(100, 74);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(614, 312);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(639, 392);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button btnSave;
+    }
+}
---
+++ DigitalSignature/Form1.cs
@@ -0,0 +1,55 @@
+namespace DigitalSignature
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        float PointX = 0;
+        float PointY = 0;
+
+        float LastX = 0;
+        float LastY = 0;
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+            g.DrawLine(Pens.Black, PointX, PointY, LastX, LastY);
+            LastX = PointX;
+            LastY = PointY;
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Reset last X & Y position to be the current mouse position when button is clicked
+            LastX = e.X;
+            LastY = e.Y;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                // Update point X & Y to current mouse position
+                PointX = e.X;
+                PointY = e.Y;
+                panel1_Paint(this, null);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            //Save as image
+            Image bmp = new Bitmap(panel1.Width, panel1.Height);
+            var gg = Graphics.FromImage(bmp);
+            var rect = panel1.RectangleToScreen(panel1.ClientRectangle);
+            gg.CopyFromScreen(rect.Location, Point.Empty, panel1.Size);
+
+            bmp.Save("D:\\OOSDDb\\BootVerhuur\\DigitalSignature\\Images\\Test.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+            Graphics g = panel1.CreateGraphics();
+            g.Clear(Color.White);
+        }
+    }
+}
