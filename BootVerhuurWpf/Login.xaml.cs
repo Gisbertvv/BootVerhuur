23cb08a8e768c12130209870e4da03ca02677141
Daan <s1168181@student.windesheim.nl>
2022-12-14 13:19:45 +0100

    Update login.xaml.cs
    User data is now available

--- BootVerhuurWpf/Login.xaml.cs
+++ BootVerhuurWpf/Login.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Windows;
 using System.Data.SqlClient;
+using System.Data;
+using Syncfusion.Pdf.Parsing;
 
 namespace BootVerhuurWpf
 {
@@ -9,6 +11,14 @@ namespace BootVerhuurWpf
     /// </summary>
     public partial class Login : Window
     {
+        public static string id;
+        public static string firstName;
+        public static string lastName;
+        public static string phoneNumber;
+        public static string email;
+        public static string boatingLevel;
+        public static string role;
+
         public Login()
         {
             InitializeComponent();
@@ -16,8 +26,6 @@ namespace BootVerhuurWpf
 
         private void btnLogin_Click(object sender, RoutedEventArgs e)
         {
-
-
             //Connect to database
             SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
             builder.DataSource = "127.0.0.1";
@@ -32,17 +40,32 @@ namespace BootVerhuurWpf
                 if (connection.State == System.Data.ConnectionState.Closed)
                 {
                     connection.Open();
-                    String query = "SELECT COUNT(1) FROM member WHERE username=@username AND password=@password";
+                    String query = "SELECT * FROM member WHERE username=@username AND password=@password";
                     SqlCommand sqlCmd = new SqlCommand(query, connection);
 
                     //Check if username and password are correct
                     sqlCmd.CommandType = System.Data.CommandType.Text;
                     sqlCmd.Parameters.AddWithValue("@username", txtUsername.Text);
                     sqlCmd.Parameters.AddWithValue("@password", txtPassword.Password);
-                    int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
 
-                    if (count == 1)
+                    //DataTable dataTable = new DataTable();
+                    DataTable userTable = new DataTable();
+
+                    userTable.Load(sqlCmd.ExecuteReader());
+
+                    if (userTable.Rows.Count == 1)
                     {
+                        id = userTable.Rows[0]["id"].ToString();
+                        firstName = userTable.Rows[0]["first_name"].ToString();
+                        lastName = userTable.Rows[0]["last_name"].ToString();
+                        phoneNumber = userTable.Rows[0]["phone_number"].ToString();
+                        email = userTable.Rows[0]["email"].ToString();
+                        boatingLevel = userTable.Rows[0]["boating_level"].ToString();
+                        role = userTable.Rows[0]["role"].ToString();
+
+                        //id = dataTable.Columns.Add("id").ColumnName;
+                        //id = dataTable.Columns.Contains();
+
                         MainWindow mainWindow = new MainWindow();
                         mainWindow.Show();
                         this.Close();
